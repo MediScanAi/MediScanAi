aba35ace6d4720942fd780ced07cde32df5dbfff
vahemac <nersesyanvahe073@gmail.com>
2025-06-09 00:58:21 +0400

    Fix: File Structure

---
+++ src/components/testsform/UrineTestForm.tsx
@@ -0,0 +1,302 @@
+import { Form, message, Select, Typography, Card, Col, Row } from 'antd';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import type { UrineTestFormValues } from '../../app/slices/testSlice';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { saveTestData, setTestData } from '../../app/slices/testSlice';
+import { auth } from '../../api/authApi';
+import { useTranslation } from 'react-i18next';
+import PrimaryButton from '../common/PrimaryButton';
+import '../../assets/styles/rootForm.css';
+import NumberInput from '../common/inputs/NumberInput';
+import SelectInput from '../common/inputs/SelectInput';
+
+const { Option } = Select;
+const { Title } = Typography;
+
+type ValueMapping = {
+  [key: string]: 'negative' | 'positive' | 'trace';
+};
+
+const UrineTestForm = () => {
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+  const dispatch = useAppDispatch();
+  const updatedData = useLocation()?.state?.urineTestData || undefined;
+  const { t } = useTranslation('urineTest');
+  const isDarkMode = useAppSelector((state) => state.theme.isDarkMode);
+  const urineTestFields = [
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.ph')}
+        </span>
+      ),
+      name: 'ph',
+      min: 4.5,
+      max: 8.0,
+      step: 0.1,
+      placeholder: t('urineTest.urineTestphPlaceholder'),
+    },
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.gravity')}
+        </span>
+      ),
+      name: 'specificGravity',
+      min: 1.0,
+      max: 1.03,
+      step: 0.001,
+      placeholder: t('urineTest.urineTestgravityPlaceholder'),
+      unit: 'g/dL',
+    },
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.protein')}
+        </span>
+      ),
+      name: 'protein',
+      min: 0,
+      max: 300,
+      step: 1,
+      placeholder: t('urineTest.urineTestproteinPlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.glucose')}
+        </span>
+      ),
+      name: 'glucose',
+      min: 0,
+      max: 1000,
+      step: 1,
+      placeholder: t('urineTest.urineTestglucosePlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.ketones')}
+        </span>
+      ),
+      name: 'ketones',
+      min: 0,
+      max: 160,
+      step: 1,
+      placeholder: t('urineTest.urineTestketonesPlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'select',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.bilirubin')}
+        </span>
+      ),
+      name: 'bilirubin',
+      options: [
+        t('urineTest.negative'),
+        t('urineTest.trace'),
+        t('urineTest.positive'),
+      ],
+      placeholder: t('urineTest.urineTestbilirubinPlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'input',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.urobilinogen')}
+        </span>
+      ),
+      name: 'urobilinogen',
+      min: 0.1,
+      max: 8.0,
+      step: 0.1,
+      placeholder: t('urineTest.urineTesturobilinogenPlaceholder'),
+      unit: 'EU/dL',
+    },
+    {
+      type: 'select',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.nitrites')}
+        </span>
+      ),
+      name: 'nitrites',
+      options: [t('urineTest.negative'), t('urineTest.positive')],
+      placeholder: t('urineTest.urineTestnitritesPlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'select',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.leukocyteEsterase')}
+        </span>
+      ),
+      name: 'leukocyteEsterase',
+      options: [
+        t('urineTest.negative'),
+        t('urineTest.trace'),
+        t('urineTest.positive'),
+      ],
+      placeholder: t('urineTest.urineTestleukocyteEsterasePlaceholder'),
+      unit: 'mg/dL',
+    },
+    {
+      type: 'select',
+      label: (
+        <span className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.blood')}
+        </span>
+      ),
+      name: 'blood',
+      options: [
+        t('urineTest.negative'),
+        t('urineTest.trace'),
+        t('urineTest.positive'),
+      ],
+      placeholder: t('urineTest.urineTestbloodPlaceholder'),
+      unit: 'mg/dL',
+    },
+  ];
+
+  const valueMapping: ValueMapping = {
+    [t('urineTest.negative')]: 'negative',
+    [t('urineTest.positive')]: 'positive',
+    [t('urineTest.trace')]: 'trace',
+  };
+
+  const onFinish = async (values: UrineTestFormValues) => {
+    const uid = auth.currentUser?.uid;
+
+    if (!uid) {
+      message.error(t('urineTest.notAuthenticated'));
+      return;
+    }
+
+    const processedValues = {
+      ...values,
+      bilirubin: valueMapping[values.bilirubin as string] || values.bilirubin,
+      nitrites: valueMapping[values.nitrites as string] || values.nitrites,
+      leukocyteEsterase:
+        valueMapping[values.leukocyteEsterase as string] ||
+        values.leukocyteEsterase,
+      blood: valueMapping[values.blood as string] || values.blood,
+      date: new Date().toISOString(),
+    };
+
+    await dispatch(
+      saveTestData({ uid, testType: 'urine', data: processedValues })
+    );
+    dispatch(setTestData({ testType: 'urine', data: processedValues }));
+
+    if (updatedData) {
+      message.success(t('urineTest.updateSuccess'));
+    } else {
+      message.success(t('urineTest.success'));
+    }
+
+    setTimeout(() => {
+      navigate('/profile/analysis-history');
+    }, 1000);
+  };
+
+  const getDisplayValue = (value: string | null) => {
+    if (!value) return null;
+    const englishValue = valueMapping[value];
+    return englishValue ? t(`urineTest.${englishValue}`) : value;
+  };
+
+  return (
+    <Card
+      className="test-card"
+      title={
+        <Title level={3} className={`input-item ${isDarkMode ? 'dark' : ''}`}>
+          {t('urineTest.title')}
+        </Title>
+      }
+    >
+      <Form
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
+        size="large"
+        initialValues={
+          updatedData
+            ? {
+                ...updatedData,
+                bilirubin: getDisplayValue(updatedData.bilirubin),
+                nitrites: getDisplayValue(updatedData.nitrites),
+                leukocyteEsterase: getDisplayValue(
+                  updatedData.leukocyteEsterase
+                ),
+                blood: getDisplayValue(updatedData.blood),
+              }
+            : undefined
+        }
+      >
+        <Row gutter={[24, 16]}>
+          {urineTestFields.map((field) => {
+            const formItem = (
+              <Form.Item
+                key={field.name}
+                label={field.label}
+                name={field.name}
+                rules={[{ required: true }]}
+              >
+                {field.type === 'input' ? (
+                  <NumberInput
+                    min={field.min}
+                    max={field.max}
+                    step={field.step}
+                    style={{ width: '100%' }}
+                    placeholder={field.placeholder}
+                  />
+                ) : (
+                  <SelectInput
+                    className={'select-item'}
+                    placeholder={t('urineTest.placeholder')}
+                  >
+                    {field.options?.map((opt) => (
+                      <Option key={opt} value={opt}>
+                        {opt.charAt(0).toUpperCase() + opt.slice(1)}
+                      </Option>
+                    ))}
+                  </SelectInput>
+                )}
+              </Form.Item>
+            );
+
+            return (
+              <Col span={12} key={field.name}>
+                {formItem}
+              </Col>
+            );
+          })}
+        </Row>
+
+        <Form.Item className="genetic-form-item">
+          <PrimaryButton
+            type="primary"
+            htmlType="submit"
+            className="submit-btn"
+          >
+            {updatedData ? t('urineTest.update') : t('urineTest.submit')}
+          </PrimaryButton>
+        </Form.Item>
+      </Form>
+    </Card>
+  );
+};
+
+export default UrineTestForm;
---
+++ src/pages/profile/AnalysisHistory.tsx
@@ -0,0 +1,537 @@
+import { Typography, Card, Divider, Row, Col } from 'antd';
+import {
+  HeartOutlined,
+  AlertOutlined,
+  FundOutlined,
+  DashboardOutlined,
+  ExperimentOutlined,
+  MedicineBoxOutlined,
+  SafetyCertificateOutlined,
+  FireOutlined,
+  EyeOutlined,
+  SmileOutlined,
+  ThunderboltOutlined,
+  SolutionOutlined,
+  SunOutlined,
+} from '@ant-design/icons';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { deleteTestData, clearTestData } from '../../app/slices/testSlice';
+import { useNavigate } from 'react-router';
+import { type TestType } from '../../app/slices/testSlice';
+import { useTranslation } from 'react-i18next';
+import { auth } from '../../api/authApi';
+import SecondaryButton from '../../components/common/SecondaryButton';
+import PrimaryButton from '../../components/common/PrimaryButton';
+import '../../assets/styles/analysis.css';
+
+const { Title, Text } = Typography;
+
+type AnalysisHistoryProps = {
+  theme: boolean;
+  width: number;
+};
+
+const AnalysisHistory: React.FC<AnalysisHistoryProps> = ({ theme, width }) => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const { t } = useTranslation('analysisHistory');
+  const bloodTestData = useAppSelector((state) => state.tests.blood);
+  const urineTestData = useAppSelector((state) => state.tests.urine);
+  const vitaminTestData = useAppSelector((state) => state.tests.vitamin);
+  const geneticTestData = useAppSelector((state) => state.tests.genetic);
+
+  const handleDeleteTest = async (testType: TestType) => {
+    const uid = auth.currentUser?.uid;
+    if (!uid) {
+      console.error('No user logged in');
+      return;
+    }
+
+    try {
+      dispatch(deleteTestData({ uid, testType }));
+      dispatch(clearTestData(testType));
+    } catch (error) {
+      console.error(`Error deleting ${testType} test:`, error);
+    }
+  };
+
+  const sendBloodTestData = () => {
+    navigate('/tests-form/blood-test', {
+      state: { bloodTestData: bloodTestData },
+    });
+  };
+
+  const sendUrineTestData = () => {
+    navigate('/tests-form/urine-test', {
+      state: { urineTestData: urineTestData },
+    });
+  };
+
+  const sendVitaminTestData = () => {
+    navigate('/tests-form/vitamin-test', {
+      state: { vitaminTestData: vitaminTestData },
+    });
+  };
+
+  const sendGeneticTestData = () => {
+    navigate('/tests-form/genetic-test', {
+      state: { geneticTestData: geneticTestData },
+    });
+  };
+
+  const testBloodResults = [
+    {
+      name: t('analysisHistory.hemoglobin'),
+      value: bloodTestData?.hemoglobin,
+      unit: t('analysisHistory.hemoglobinUnit'),
+      normalRange: '13.5-17.5',
+      icon: <HeartOutlined />,
+    },
+    {
+      name: t('analysisHistory.whiteBloodCells'),
+      value: bloodTestData?.wbc,
+      unit: t('analysisHistory.whiteBloodCellsUnit'),
+      normalRange: '4.5-11.0',
+      icon: <AlertOutlined />,
+    },
+    {
+      name: t('analysisHistory.redBloodCells'),
+      value: bloodTestData?.rbc,
+      unit: t('analysisHistory.redBloodCellsUnit'),
+      normalRange: '4.5-5.9',
+      icon: <FundOutlined />,
+    },
+    {
+      name: t('analysisHistory.platelets'),
+      value: bloodTestData?.platelets,
+      unit: t('analysisHistory.plateletsUnit'),
+      normalRange: '150-450',
+      icon: <DashboardOutlined />,
+    },
+    {
+      name: t('analysisHistory.glucose'),
+      value: bloodTestData?.glucose,
+      unit: t('analysisHistory.glucoseUnit'),
+      normalRange: '70-99',
+      icon: <ExperimentOutlined />,
+    },
+    {
+      name: t('analysisHistory.cholesterol'),
+      value: bloodTestData?.cholesterol,
+      unit: t('analysisHistory.cholesterolUnit'),
+      normalRange: '<200',
+      icon: <HeartOutlined />,
+    },
+  ];
+
+  const testUrineResults = [
+    {
+      name: t('analysisHistory.ph'),
+      value: urineTestData?.ph,
+      unit: t('analysisHistory.phUnit'),
+      normalRange: '4.5-7.5',
+      icon: <ExperimentOutlined />,
+    },
+    {
+      name: t('analysisHistory.specificGravity'),
+      value: urineTestData?.specificGravity,
+      unit: t('analysisHistory.specificGravityUnit'),
+      normalRange: '1.005-1.030',
+      icon: <DashboardOutlined />,
+    },
+    {
+      name: t('analysisHistory.protein'),
+      value: urineTestData?.protein,
+      unit: t('analysisHistory.proteinUnit'),
+      normalRange: '0-150',
+      icon: <MedicineBoxOutlined />,
+    },
+    {
+      name: t('analysisHistory.glucose'),
+      value: urineTestData?.glucose,
+      unit: t('analysisHistory.glucoseUnit'),
+      normalRange: '50-100',
+      icon: <ExperimentOutlined />,
+    },
+    {
+      name: t('analysisHistory.ketones'),
+      value: urineTestData?.ketones,
+      unit: t('analysisHistory.ketonesUnit'),
+      normalRange: '0-5',
+      icon: <FireOutlined />,
+    },
+    {
+      name: t('analysisHistory.bilirubin'),
+      value: urineTestData?.bilirubin,
+      unit: t('analysisHistory.bilirubinUnit'),
+      normalRange: '0.3-1.0',
+      icon: <MedicineBoxOutlined />,
+    },
+    {
+      name: t('analysisHistory.urobilinogen'),
+      value: urineTestData?.urobilinogen,
+      unit: t('analysisHistory.urobilinogenUnit'),
+      normalRange: '0-10',
+      icon: <DashboardOutlined />,
+    },
+    {
+      name: t('analysisHistory.nitrite'),
+      value: urineTestData?.nitrites,
+      unit: t('analysisHistory.nitriteUnit'),
+      normalRange: '0-10',
+      icon: <MedicineBoxOutlined />,
+    },
+    {
+      name: t('analysisHistory.leukocyteEsterase'),
+      value: urineTestData?.leukocyteEsterase,
+      unit: t('analysisHistory.leukocyteEsteraseUnit'),
+      normalRange: '0-10',
+      icon: <ExperimentOutlined />,
+    },
+    {
+      name: t('analysisHistory.blood'),
+      value: urineTestData?.blood,
+      unit: t('analysisHistory.bloodUnit'),
+      normalRange: '0-10',
+      icon: <HeartOutlined />,
+    },
+  ];
+
+  const testVitaminResults = [
+    {
+      name: t('analysisHistory.vitaminA'),
+      value: vitaminTestData?.vitaminA,
+      unit: t('analysisHistory.vitaminAUnit'),
+      normalRange: '0-10',
+      icon: <EyeOutlined />,
+    },
+    {
+      name: t('analysisHistory.vitaminB12'),
+      value: vitaminTestData?.vitaminB12,
+      unit: t('analysisHistory.vitaminB12Unit'),
+      normalRange: '0-10',
+      icon: <ThunderboltOutlined />,
+    },
+    {
+      name: t('analysisHistory.vitaminC'),
+      value: vitaminTestData?.vitaminC,
+      unit: t('analysisHistory.vitaminCUnit'),
+      normalRange: '0-10',
+      icon: <SmileOutlined />,
+    },
+    {
+      name: t('analysisHistory.vitaminD'),
+      value: vitaminTestData?.vitaminD,
+      unit: t('analysisHistory.vitaminDUnit'),
+      normalRange: '0-10',
+      icon: <SunOutlined />,
+    },
+    {
+      name: t('analysisHistory.vitaminE'),
+      value: vitaminTestData?.vitaminE,
+      unit: t('analysisHistory.vitaminEUnit'),
+      normalRange: '0-10',
+      icon: <MedicineBoxOutlined />,
+    },
+    {
+      name: t('analysisHistory.vitaminK'),
+      value: vitaminTestData?.vitaminK,
+      unit: t('analysisHistory.vitaminKUnit'),
+      normalRange: '0-10',
+      icon: <SolutionOutlined />,
+    },
+  ];
+
+  const testGeneticResults = [
+    {
+      name: t('analysisHistory.brca1'),
+      value: geneticTestData?.brca1,
+      icon: <HeartOutlined />,
+    },
+    {
+      name: t('analysisHistory.brca2'),
+      value: geneticTestData?.brca2,
+      icon: <HeartOutlined />,
+    },
+    {
+      name: t('analysisHistory.apoe'),
+      value: geneticTestData?.apoe,
+      icon: <SafetyCertificateOutlined />,
+    },
+    {
+      name: t('analysisHistory.mthfr'),
+      value: geneticTestData?.mthfr,
+      icon: <SafetyCertificateOutlined />,
+    },
+    {
+      name: t('analysisHistory.factorVLeiden'),
+      value: geneticTestData?.factor_v_leiden,
+      icon: <SafetyCertificateOutlined />,
+    },
+    {
+      name: t('analysisHistory.cyp2c19'),
+      value: geneticTestData?.cyp2c19,
+      icon: <SafetyCertificateOutlined />,
+    },
+  ];
+
+  const renderTestCards = (
+    testArray: {
+      name: string;
+      value: string;
+      unit: string;
+      normalRange: string;
+      icon: React.ReactNode;
+    }[]
+  ) => {
+    return (
+      <div className="task-card">
+        <Row gutter={[24, 16]} className="info-grid">
+          {testArray.map(
+            (test, index) =>
+              test.value !== undefined &&
+              test.value !== null &&
+              test.value !== '' && (
+                <Col xs={24} sm={12} md={8} key={index}>
+                  <div className={`info-card ${theme ? 'dark' : ''}`}>
+                    <div className="info-label">
+                      {test.icon}
+                      <Text className="info-label-text" strong>
+                        {test.name}
+                      </Text>
+                    </div>
+
+                    <div className="view-mode-container">
+                      <div className="info-value">
+                        <Text className="info-value-text">
+                          {test.value}
+                          {test.unit && (
+                            <span className="unit"> ({test.unit})</span>
+                          )}
+                        </Text>
+                      </div>
+                    </div>
+
+                    {test.normalRange && (
+                      <div className="info-value">
+                        <Text className="info-value-text" type="secondary">
+                          Normal: {test.normalRange}
+                        </Text>
+                      </div>
+                    )}
+                  </div>
+                </Col>
+              )
+          )}
+        </Row>
+      </div>
+    );
+  };
+
+  const hasAnyTestData =
+    bloodTestData || urineTestData || vitaminTestData || geneticTestData;
+
+  return (
+    <div className="history-container">
+      <Col>
+        <Card className={`analysis-card ${theme ? 'dark-mode-text' : ''}`}>
+          <Title
+            level={2}
+            className="analysis-title"
+            style={{ fontSize: width < 800 ? '10px' : '32px' }}
+          >
+            {t('analysisHistory.title')}
+          </Title>
+
+          {!hasAnyTestData && (
+            <Text
+              type="secondary"
+              className="no-test-results"
+              style={{ fontSize: width < 800 ? '10px' : '20px' }}
+            >
+              {t('analysisHistory.noTestResults')}
+            </Text>
+          )}
+
+          <Row gutter={[24, 32]}>
+            {bloodTestData && (
+              <Col span={20}>
+                <div className="test-header">
+                  <Text
+                    strong
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.bloodTest')}
+                  </Text>
+                  <Text
+                    type="secondary"
+                    className="last-updated"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.lastUpdated')}
+                    {bloodTestData.date ||
+                      t('analysisHistory.dateNotSpecified')}
+                  </Text>
+                  <PrimaryButton
+                    onClick={sendBloodTestData}
+                    className="edit-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.edit')}
+                  </PrimaryButton>
+                  <SecondaryButton
+                    onClick={() => handleDeleteTest('blood')}
+                    className="delete-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.delete')}
+                  </SecondaryButton>
+                </div>
+                <Divider className="test-divider" />
+                {renderTestCards(
+                  testBloodResults.map((result) => ({
+                    ...result,
+                    value: result.value?.toString() || '',
+                  }))
+                )}
+              </Col>
+            )}
+
+            {urineTestData && (
+              <Col span={20}>
+                <div className="test-header">
+                  <Text
+                    strong
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.urineTest')}
+                  </Text>
+                  <Text
+                    type="secondary"
+                    className="last-updated"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.lastUpdated')}
+                    {urineTestData.date ||
+                      t('analysisHistory.dateNotSpecified')}
+                  </Text>
+                  <PrimaryButton
+                    onClick={sendUrineTestData}
+                    className="edit-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.edit')}
+                  </PrimaryButton>
+                  <SecondaryButton
+                    onClick={() => handleDeleteTest('urine')}
+                    className="delete-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.delete')}
+                  </SecondaryButton>
+                </div>
+                <Divider className="test-divider" />
+                {renderTestCards(
+                  testUrineResults.map((result) => ({
+                    ...result,
+                    value: result.value?.toString() || '',
+                  }))
+                )}
+              </Col>
+            )}
+
+            {vitaminTestData && (
+              <Col span={20}>
+                <div className="test-header">
+                  <Text
+                    strong
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.vitaminTest')}
+                  </Text>
+                  <Text
+                    type="secondary"
+                    className="last-updated"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.lastUpdated')}
+                    {vitaminTestData.date ||
+                      t('analysisHistory.dateNotSpecified')}
+                  </Text>
+                  <PrimaryButton
+                    onClick={sendVitaminTestData}
+                    className="edit-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.edit')}
+                  </PrimaryButton>
+                  <SecondaryButton
+                    onClick={() => handleDeleteTest('vitamin')}
+                    className="delete-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.delete')}
+                  </SecondaryButton>
+                </div>
+                <Divider className="test-divider" />
+                {renderTestCards(
+                  testVitaminResults.map((result) => ({
+                    ...result,
+                    value: result.value?.toString() || '',
+                  }))
+                )}
+              </Col>
+            )}
+
+            {geneticTestData && (
+              <Col span={20}>
+                <div className="test-header">
+                  <Text
+                    strong
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.geneticTest')}
+                  </Text>
+                  <Text
+                    type="secondary"
+                    className="last-updated"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.lastUpdated')}
+                    {geneticTestData.date ||
+                      t('analysisHistory.dateNotSpecified')}
+                  </Text>
+                  <PrimaryButton
+                    onClick={sendGeneticTestData}
+                    className="edit-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.edit')}
+                  </PrimaryButton>
+                  <SecondaryButton
+                    onClick={() => handleDeleteTest('genetic')}
+                    className="delete-button"
+                    style={{ fontSize: width < 800 ? '10px' : '16px' }}
+                  >
+                    {t('analysisHistory.delete')}
+                  </SecondaryButton>
+                </div>
+                <Divider className="test-divider" />
+                {renderTestCards(
+                  testGeneticResults.map((result) => ({
+                    ...result,
+                    value: result.value?.toString() || '',
+                    unit: '',
+                    normalRange: '',
+                  }))
+                )}
+              </Col>
+            )}
+          </Row>
+        </Card>
+      </Col>
+    </div>
+  );
+};
+
+export default AnalysisHistory;
